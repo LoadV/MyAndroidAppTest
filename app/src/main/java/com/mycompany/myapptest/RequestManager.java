b83914853d4c62142ecffb3fb21ad2f65b5fdc25
LoadV <1409125153@qq.com>
2017-01-09 19:13:19 +0800

    😔

---
+++ app/src/main/java/com/mycompany/myapptest/RequestManager.java
@@ -0,0 +1,17 @@
+package com.mycompany.myapptest;
+
+public class RequestManager
+{
+	private static final MediaType MEDIA_TYPE_JSON = MediaType.parse("application/x-www-form-urlencoded; charset=utf-8");//mdiatype 这个需要和服务端保持一致
+    private static final MediaType MEDIA_TYPE_MARKDOWN = MediaType.parse("text/x-markdown; charset=utf-8");//mdiatype 这个需要和服务端保持一致
+    private static final String TAG = RequestManager.class.getSimpleName();
+    private static final String BASE_URL = "http://xxx.com/openapi";//请求接口根地址
+    private static volatile RequestManager mInstance;//单利引用
+    public static final int TYPE_GET = 0;//get请求
+    public static final int TYPE_POST_JSON = 1;//post请求参数为json
+    public static final int TYPE_POST_FORM = 2;//post请求参数为表单
+    private OkHttpClient mOkHttpClient;//okHttpClient 实例
+    private Handler okHttpHandler;//全局处理子线程和M主线程通信
+
+	
+}
